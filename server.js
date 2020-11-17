2bb83559e5f944bf7a7506787c8439e4e968b582
AsgharB <abhutta@Asghar.local>
2020-11-16 21:42:45 -0500

    uploooad

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+//serves up static content such as css files and js files in a folder
+app.use(express.static('public'));
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -98,6 +101,18 @@ app.get('/api/animals/:id', (req, res) => {
     }else{
         res.send(404);
     }  
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
   });
 
   app.post('/api/animals', (req, res) => {
